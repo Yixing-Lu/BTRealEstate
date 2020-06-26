5275495811a69a6996ea29908ddcbda6abc79e2a
Yixing-Lu <yixinglu97@gmail.com>
2020-06-26 15:07:40 -0700

    disable email

--- contacts/views.py
+++ contacts/views.py
@@ -24,13 +24,13 @@ def contact(request):
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
 
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info.',
-            'yixinglu97@gmail.com',
-            [realtor_email,'yixinglu97@gmail.com'],
-            fail_silently=False
-        )
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info.',
+        #     'yixinglu97@gmail.com',
+        #     [realtor_email,'yixinglu97@gmail.com'],
+        #     fail_silently=False
+        # )
 
         messages.success(request, 'Your requeset has been submitted, a realtor will get back to you soon')
         return redirect('/listings/' + listing_id)
